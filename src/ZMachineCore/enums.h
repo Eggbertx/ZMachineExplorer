36c870d3d47d63df961ba2057b683795d24a9a5b
Eggbertx <eggbertx@users.noreply.github.com>
2026-05-15 16:59:02 -0700

    Move enums to separate header

---
+++ src/ZMachineCore/enums.h
@@ -0,0 +1,99 @@
+namespace ZMachineCore {
+
+enum HeaderAddress {
+    Version,
+    Flags1,
+    HighMemoryBase = 0x4,
+    InitialPC = 0x6,
+    DictionaryAddr = 0x8,
+    ObjectsAddr = 0xa,
+    GlobalsAddr = 0xc,
+    StaticAddr = 0xe,
+    Flags2 = 0x10,
+    AbbreviationsAddr = 0x18,
+    FileLength = 0x1a,
+    Checksum = 0x1c,
+    InterpreterNum = 0x1e,
+    InterpreterRev,
+    ScreenHeightLines,
+    ScreenWidthChars,
+    ScreenWidthUnits,
+    ScreenHeightUnits = 0x24,
+    FontWidth = 0x26,
+    FontHeight,
+    RoutinesOffset,
+    StringsOffset = 0x2a,
+    DefaultBG = 0x2c,
+    DefaultFG,
+    TerminatingCharsTableAddr = 0x2e,
+    Stream3Width = 0x30,
+    StandardRevNum = 0x32,
+    AlphabetTableAddr = 0x34,
+    HeaderExtTableAddr = 0x36
+};
+
+enum Flag1Bits {
+    ColorsAvailable,
+    PictureDisplayAvailable,
+    BoldFaceAvailable,
+    ItalicAvailable,
+    FixedSpaceStyleAvailable,
+    SoundEffectsAvailable,
+    TimedKeyInputAvailable = 7
+};
+
+enum Flag2Bits {
+    TranscriptingEnabled,
+    ForceFixedPitchPrinting,
+    ScreenRedrawRequested,
+    GameWantsPictures,
+    GameWantsUndo,
+    GameWantsMouse,
+    GameWantsColors,
+    GameWantsSFX,
+    GameWantsMenus
+};
+
+enum InterpreterNum {
+    Unknown,
+    DECSystem20,
+    AppleIIe,
+    Macintosh,
+    Amiga,
+    AtariST,
+    IBMPC,
+    Commodore128,
+    Commodore64,
+    AppleIIc,
+    AppleIIgs,
+    TandyColor
+};
+
+enum MemoryRegionType {
+    UnknownMemory,
+    DynamicMemory, // [0, StaticAddr)
+    StaticMemory,  // [StaticAddr, HighMemoryBase)
+    HighMemory     // [HighMemoryBase, end)
+};
+
+enum MemoryWriteSource { UnknownSource, StorySource, InterpreterSource, ResetSource };
+
+enum ColourCode {
+    PixelUnderCursor = -1,
+    Current,
+    Default,
+    Black,
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+    LightGrey,
+    MediumGrey,
+    DarkGrey,
+    Transparent = 15
+};
+
+} // namespace ZMachineCore
